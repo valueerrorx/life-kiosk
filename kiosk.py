da9c4d72abd1f17061564f6acb1776716c2b34dd
valueerrorx <valueerror@gmail.com>
2018-01-31 22:42:54 +0100

    major ui changes

--- kiosk.py
+++ kiosk.py
@@ -20,17 +20,29 @@ class MeinDialog(QtWidgets.QDialog):
         QtWidgets.QDialog.__init__(self)
         scriptdir=os.path.dirname(os.path.abspath(__file__))
         uifile=os.path.join(scriptdir,'kiosk.ui')
-        winicon=os.path.join(scriptdir,'images/frostwire.png')
+        winicon=os.path.join(scriptdir,'images/kiosk.png')
         
         self.ui = uic.loadUi(uifile)        # load UI
         self.ui.setWindowIcon(QIcon(winicon))
         self.ui.exit.clicked.connect(self.onAbbrechen)        # setup Slots
         self.ui.start.clicked.connect(self.onStartConfig)
 
-       
-        self.onReadConfig()
+        self.configfiles = []
+        self.getConfigFiles()
+        self.readConfig()
+     
         
     
+
+    def getConfigFiles(self):
+        
+        for root, dirs, files in os.walk("kiosk/"):
+            for name in files:
+                if name.endswith((".kiosk")):
+                     self.configfiles.append(os.path.join(root, name))
+        
+
+
 
 
 
@@ -56,71 +68,62 @@ class MeinDialog(QtWidgets.QDialog):
     
 
 
-    def onReadConfig(self):
-         
-
-        generalgrid = self.createGrid("kiosk/general.kiosk")
-        self.ui.generalbox.setLayout(generalgrid)
-         
-        filegrid = self.createGrid("kiosk/filebrowsing.kiosk")
-        self.ui.browsingbox.setLayout(filegrid)
-         
-
-        actionsgrid = self.createGrid("kiosk/actions.kiosk")
-        self.ui.actionsbox.setLayout(actionsgrid)
-        
-        settingsgrid = self.createGrid("kiosk/settings.kiosk")
-        self.ui.settingsbox.setLayout(settingsgrid)
-        
-        themesgrid = self.createGrid("kiosk/themes.kiosk")
-        self.ui.themesbox.setLayout(themesgrid)
+    def readConfig(self):
+        for configfile in self.configfiles:
+            generalgrid,groupname,sectionicon = self.createGrid(configfile)
+            tab = QtWidgets.QWidget()
+           
+            tab.setLayout(generalgrid)
+            self.ui.tabWidget.addTab(tab, sectionicon, groupname)
+            
 
     
     
     def createGrid(self, configfile):
         """
         this section reads the config.kiosk file 
-        and creates qtwidgets for every action 
+        and creates tabs for every configfile and qtwidgets for every action 
         in the config file
-        returns: gridlayout
+        returns: maingrid, groupname
         """
-       
         self.Config = ConfigParser.ConfigParser()
         self.Config.read(configfile)
         sections = self.Config.sections()
-        
-       
-        
-        maingrid = QtWidgets.QGridLayout()
+
+        maingrid = QtWidgets.QGridLayout()   #this is the mainlayout that is returned and later applied to the tab 
+        scrollarea = QtWidgets.QScrollArea()  # scrollarea is the only widget inside the mainlayout
+        scrollgrid = QtWidgets.QGridLayout()  # scrollgrid is the gridlayout inside the scroallarea
+        scrollwidget = QtWidgets.QWidget()  #scrollwidget is the first widget for the scrollarea and gets scrollgrid as layout
+        scrollarea.setFrameShape(QtWidgets.QFrame.NoFrame)
+ 
         widgets = []
         for section in sections:
             if section == "Group":
                 try:
                     groupicon = self.ConfigSectionMap(section)['icon']
+                    groupname = self.ConfigSectionMap(section)['name']
                     groupdesc = self.ConfigSectionMap(section)['description']
                 except KeyError:
                     print("key not found")
-                
+       
+                sectionicon = QIcon.fromTheme(groupicon)
                 itemicon = QtWidgets.QLabel()
-                itemicon.setPixmap(QPixmap("images/%s" % groupicon))
+                itemicon.setPixmap(QPixmap(sectionicon.pixmap(64)))
+
                 itemicon.setStyleSheet("margin-left:-4px;")
-                
                 itemdesc = QtWidgets.QLabel()
                 itemdesc.setText(groupdesc)
-                itemplaceholder= QtWidgets.QLabel('      ')
+                verticalSpacer = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
                 
                 grid = QtWidgets.QGridLayout()
                 grid.setSpacing(2)
-                grid.setRowStretch (0, 0)
                 grid.addWidget(itemicon, 0, 0)
                 grid.addWidget(itemdesc, 1, 0)
-                grid.addWidget(itemplaceholder, 2, 0)
-                
+                grid.addItem(verticalSpacer, 2, 0)
+
                 titlewidget = QtWidgets.QWidget()
                 titlewidget.setLayout(grid)
-                
-                maingrid.addWidget(titlewidget)
-               
+                scrollgrid.addWidget(titlewidget,0,0)
                 
             else:
                 try:
@@ -133,41 +136,43 @@ class MeinDialog(QtWidgets.QDialog):
                 
                 itemname = QtWidgets.QLabel()
                 itemname.setText("<b>%s</b>" % sectionname)
-                itemname.setSizePolicy(QtWidgets.QSizePolicy.Expanding,QtWidgets.QSizePolicy.Expanding);
+                itemname.setSizePolicy(QtWidgets.QSizePolicy.Expanding,QtWidgets.QSizePolicy.Expanding)
                 itemname.setAlignment(QtCore.Qt.AlignLeft)
                 itemname.setStyleSheet("margin-top:1px;")
                 
                 itemdesc = QtWidgets.QLabel()
+                itemdesc.setSizePolicy(QtWidgets.QSizePolicy.Expanding,QtWidgets.QSizePolicy.Expanding)
                 itemdesc.setText(sectiondesc)
                 itemdesc.setWordWrap(True);
-                itemdesc.setSizePolicy(QtWidgets.QSizePolicy.Expanding,QtWidgets.QSizePolicy.Expanding);
-                
+                itemdesc.setStyleSheet("margin-left:1px;")
+                itemdesc.setAlignment(QtCore.Qt.AlignTop)
+    
                 itemcheckBox = QtWidgets.QCheckBox()
                 itemcheckBox.setStyleSheet("margin-left:-2px;")
-                itemplaceholder= QtWidgets.QLabel('      ')
-                    
+              
                 grid = QtWidgets.QGridLayout()
                 grid.setSpacing(0)
-                
-                grid.setRowStretch (0, 0)
-                grid.addWidget(itemname, 0, 1)
+                grid.setColumnMinimumWidth(1,600)
                 grid.addWidget(itemcheckBox, 0, 0)
-                grid.addWidget(itemdesc, 2, 0, 2, 0 )  #fromRow = 0, fromColumn = 0, rowSpan = 2 and columnSpan = 0
-                grid.addWidget(itemplaceholder, 3, 0)
-                #grid.setColumnStretch(0, 3)
-                #grid.setRowStretch(1, 0)
-                
+                grid.addWidget(itemname, 0, 1)
+                grid.addWidget(itemdesc, 1, 1 )  #fromRow = 0, fromColumn = 0, rowSpan = 2 and columnSpan = 0
+
                 widget = QtWidgets.QWidget()
+                widget.setSizePolicy(QtWidgets.QSizePolicy.Minimum,QtWidgets.QSizePolicy.Minimum)
                 widget.setLayout(grid)
-                
                 widgets.append(widget)
 
 
         for widget in widgets:
-            maingrid.addWidget(widget)
-        
-        return maingrid
+            scrollgrid.addWidget(widget)
 
+        scrollwidget.setLayout(scrollgrid)
+        scrollarea.setWidget(scrollwidget)
+        maingrid.addWidget(scrollarea,0,0)
+        
+        return maingrid, groupname, sectionicon
+
+    
     
     def onAbbrechen(self):    # Exit button
         self.ui.close()
