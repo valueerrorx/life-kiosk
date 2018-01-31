046aeaf65be9bcef5c9281349e30e1dd56b6bce5
valueerrorx <valueerror@gmail.com>
2018-01-31 22:52:19 +0100

    added comments

--- kiosk.py
+++ kiosk.py
@@ -1,5 +1,13 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
+#
+# Copyright (C) 2018 Thomas Michael Weissel
+#
+# This software may be modified and distributed under the terms
+# of the GPLv3 license.  See the LICENSE file for details.
+
+
+
 import sys, os
 import socket
 from PyQt5 import QtCore, uic, QtWidgets
